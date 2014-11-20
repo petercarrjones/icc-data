445fd15dfd65b9223c69a745e7d939dfa56a77e4
Peter Jones <pjones15@gmu.edu>
2014-11-19 23:59:57 -0500

    parse sample table
    Able to parse out the correct table feature in a sample table. Still very messy.

--- table.r
+++ table.r
@@ -1,1 +1,15 @@
 #load and parse html tables
+
+#load libraries
+library(stringr)
+library(XML)
+library(magrittr)
+
+#Parse a sample table
+doc <-"table/v02toc.html"
+
+p <- htmlParse(doc)
+
+iccv02toc <- readHTMLTable(p, header = FALSE, stringsAsFactors = FALSE)
+str(iccv02toc)
+write.csv(iccv02toc[4], file = "out/iccv02toc.csv")
