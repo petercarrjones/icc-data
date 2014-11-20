e024a992dcdc8913572880ce778b5a830180225f
Peter Jones <pjones15@gmu.edu>
2014-11-19 22:19:19 -0500

    Add get_real_words function to remove non-words
    Change previous function which worked on n-grams to work on icc_texts list.

--- load.r
+++ load.r
@@ -16,7 +16,10 @@ names(raw) <- files
 icc_texts <- lapply(raw, paste, collapse = " ") %>%
   lapply(., tolower) %>%
   lapply(., WordTokenizer) %>%
-  lapply(., paste, collapse = " ")
+  lapply(., get_real_words)
+
+
+  iclapply(., paste, collapse = " ")
 
 #Create an N-gram maker with Rweka's function
 ngrammify <- function(data, n) { 
@@ -24,20 +27,19 @@ ngrammify <- function(data, n) {
 }
 
 #Turn text list into N-grams, in this case 5-grams
-icc_grams <- lapply(icc_texts, ngrammify, 5)
-every_grams <- icc_grams %>% unlist() %>% unique()
+#icc_grams <- lapply(icc_texts, ngrammify, 5)
+#every_grams <- icc_grams %>% unlist() %>% unique()
 
 
 
 #Remove non-words from the raw icc texts
-filter_unreasonable_ngrams <- function(ngrams) {
-  require(stringr)
-  ngrams[!str_detect(ngrams, "[^a-z ]")]
+get_real_words <- function(word) {
+  word[!stringr::str_detect(word, "[^a-z ]")]
 }
 
 #Run the filter_unreasonable_ngrams function
-fix_grams <- filter_unreasonable_ngrams(every_grams)
+fix_grams <- filter_unreasonable_ngrams(icc_texts)
 head(fix_grams)
 
 #Save the data
-write.csv(fix_grams, file = "out/every_ngram_fix.csv")
+#write.csv(icc_texts, file = "out/icc_texts.csv")
